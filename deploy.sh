439d028efb872227a2a737d68242c7f1bbb02b24
IaniCristi <iany_m5@yahoo.com>
2021-08-17 15:53:21 +0300

    mesaj

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 docker build -t stephengrider/multi-client -f ./client/Dockerfile ./client
 docker build -t stephengrider/multi-server -f ./server/Dockerfile ./server
 docker build -t stephengrider/multi-worker -f ./worker/Dockerfile ./worker
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 kubectl apply -f k8s 
