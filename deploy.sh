b9e40e9084fd02530ab8c8d1123785cbe0e731d9
IaniCristi <iany_m5@yahoo.com>
2021-08-20 15:18:27 +0300

    commit

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ docker build -t stephengrider/multi-client -f ./client/Dockerfile ./client
 docker build -t stephengrider/multi-server -f ./server/Dockerfile ./server
 docker build -t stephengrider/multi-worker -f ./worker/Dockerfile ./worker
 #curl -u iani:1137055645a9fff6e5f8740fa7be2dda6d "http://localhost:8080/job/k8s/"
+kubectl version
 kubectl config set-context minikube
-kubectl apply -f k8s --token $Kubernetes --server apiserver.hostname.local
+kubectl apply -f k8s --token Kubernetes --server apiserver.hostname.local
