52738ff8ac7cc65cdb02eb1abdfcf448b45e6682
IaniCristi <iany_m5@yahoo.com>
2021-08-23 17:10:56 +0300

    commit

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ docker build -t stephengrider/multi-client -f ./client/Dockerfile ./client
 docker build -t stephengrider/multi-server -f ./server/Dockerfile ./server
 docker build -t stephengrider/multi-worker -f ./worker/Dockerfile ./worker
 #curl -u iani:1137055645a9fff6e5f8740fa7be2dda6d "http://localhost:8080/job/k8s/"
-minikube start
-kubectl apply -f k8s
+kubectl --kubeconfig=config config set-context --current --user=jenkins
+kubectl --kubeconfig=config apply -f k8s
