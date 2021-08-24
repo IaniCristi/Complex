b0a3528c3d33ef55d11807ae01fa2de408545c10
IaniCristi <iany_m5@yahoo.com>
2021-08-24 13:27:15 +0300

    commit

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ docker build -t stephengrider/multi-server -f ./server/Dockerfile ./server
 docker build -t stephengrider/multi-worker -f ./worker/Dockerfile ./worker
 #curl -u iani:1137055645a9fff6e5f8740fa7be2dda6d "http://localhost:8080/job/k8s/"
 kubectl --kubeconfig=config config set-context --current --user=minikube
+kubectl --kubeconfig=config apply -f pgpassword.yaml
 kubectl --kubeconfig=config apply -f k8s
